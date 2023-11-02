53f5c29932b9018ce8c511a58752ace9bd2e3fc8
ClemersonAssuncao <clemerson.m.b@gmail.com>
2023-11-02 19:17:22 -0300

    Add edit create Register

--- static/BuscaSemantica/js/models.js
+++ static/BuscaSemantica/js/models.js
@@ -11,8 +11,47 @@ Folder.prototype = {
         nameFolder = this.form.querySelector('*[name="name"]').value
         parent_id = this.form.querySelector('*[name="id_parent_dir"]').value
 
-        this.service.addSaveFolder(id, nameFolder, parent_id).then( (botText) => {
+        this.service.addSaveFolder(id, nameFolder, parent_id).then( (data) => {
+            
+            const jsonReturn = JSON.parse(data);
+            if (jsonReturn.created){
+                const dropdown = document.createElement('li');
+                dropdown.className = 'mb-1';
+                dropdown.setAttribute('style',"margin-left:10px")
+                dropdown.setAttribute('data-id', jsonReturn.id)
+                dropdown.innerHTML  = `<div class="dropdown-context-menu" data-option="#context-menu-folder">
+                                            <button class="btn btn-toggle align-items-center rounded " data-bs-toggle="collapse" data-bs-target="#folder-${jsonReturn.id}" aria-expanded="false">
+                                                <i class="bi bi-caret-right"></i> 
+                                            </button>
+                                            <span role="button" class="selection-element folder" data-selection=".folder" data-id="${jsonReturn.id}">${nameFolder}</span>
+                                        </div>
+                                        <div class="collapse show" id="folder-${jsonReturn.id}">
+                                            <ul class="list-unstyled btn-toggle-nav fw-normal pb-1">
+                                            
+                                            </ul>
+                                        </div>`
+                this.tree.querySelector(`li[data-id='${parent_id}'] ul`).appendChild(dropdown)
+            } else {
+                
+            }
+            
+            botText.id
             console.log(botText)
         });
     },
 }
+/*
+<li class="mb-1" style="margin-left:10px" data-id="9">
+            <div class="dropdown-context-menu" data-option="#context-menu-folder">
+                <button class="btn btn-toggle align-items-center rounded " data-bs-toggle="collapse" data-bs-target="#folder-9" aria-expanded="false">
+                    <i class="bi bi-caret-right"></i> 
+                </button>
+                <span role="button" class="selection-element folder" data-selection=".folder" data-id="9">pasta 2.1.1</span>
+            </div>
+            <div class="collapse show" id="folder-9">
+                <ul class="list-unstyled btn-toggle-nav fw-normal pb-1">
+                
+                </ul>
+            </div>
+        </li>
+            */
--- static/BuscaSemantica/js/script.js
+++ static/BuscaSemantica/js/script.js
@@ -1,6 +1,7 @@
 document.querySelectorAll('.dropdown-context-menu').forEach( element => {
     element.addEventListener('contextmenu', function(e) {
         e.preventDefault();
+        document.querySelector('#add_edit_folder input[name="id"]').value = e.currentTarget.parentElement.getAttribute('data-id');
         const contextMenu = document.querySelector(element.getAttribute('data-option'));
         contextMenu.classList.add('show')
         if (document.body.clientHeight < contextMenu.clientHeight + e.screenY ){
@@ -29,4 +30,29 @@ document.querySelector('.context-menu').addEventListener('mouseleave', (e) => {
     e.fromElement.classList.remove('show')
 })
 
+function addEditFolder(ev){
+    folder = new Folder(ev.srcElement, document.querySelector('#folder-tree'))
+    folder.addSaveFolder().then( data => {
+        console.log(data)
+    });
+    return false
+}
+
+document.querySelector('#add_edit_folder').addEventListener('shown.bs.modal', (ev) => {
+    ev.srcElement.querySelector('#folder-action-title').textContent = ev.relatedTarget.getAttribute('data-action-title');
+})
+
+document.querySelectorAll('.folder').forEach( element => {
+    element['folder-filter'] = document.querySelector('#filter-folder')
+    element.addEventListener('click', function(e) {
+        const folder = document.createElement('span');
+        folder.className = 'badge rounded-pill text-bg-secondary text-white mx-1';
+        folder.setAttribute('role','button');
+        folder.setAttribute('data-id', element.getAttribute('data-id'));
+        folder.textContent = element.textContent;
+        folder.innerHTML += '<i class="bi bi-x"></i>'
+        folder.addEventListener('click', () => folder.remove())
+        element['folder-filter'].appendChild(folder);
+    });
+});
 
